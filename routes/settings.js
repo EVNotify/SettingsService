7990d34bb9fd0b716e4a7c81ac3feaaa502f439b
GPlay97 <GPlay97@users.noreply.github.com>
2020-10-13 21:50:56 +0200

    Remove slash on route

--- routes/settings.js
+++ routes/settings.js
@@ -6,7 +6,7 @@ const authorizationMiddleware = require('@evnotify/middlewares').authorizationHa
 const settingsController = require('../controllers/settings');
 
 router.get('/:akey', authorizationMiddleware, authenticationMiddleware, settingsController.getSettings);
-router.patch('/:akey/', authorizationMiddleware, authenticationMiddleware, settingsController.updateSettings);
+router.patch('/:akey', authorizationMiddleware, authenticationMiddleware, settingsController.updateSettings);
 router.delete('/:akey', authorizationMiddleware, authenticationMiddleware, settingsController.deleteSettings);
 
 module.exports = router;
